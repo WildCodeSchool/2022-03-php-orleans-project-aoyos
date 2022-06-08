2cd3e4fbdecadc9ef6d295372461c4696177cf67
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-08 12:54:23 +0200

    make crud

---
+++ src/Controller/AdminProductionController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Production;
+use App\Form\Production1Type;
+use App\Repository\ProductionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/production')]
+class AdminProductionController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_production_index', methods: ['GET'])]
+    public function index(ProductionRepository $productionRepository): Response
+    {
+        return $this->render('admin_production/index.html.twig', [
+            'productions' => $productionRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_production_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProductionRepository $productionRepository): Response
+    {
+        $production = new Production();
+        $form = $this->createForm(Production1Type::class, $production);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productionRepository->add($production, true);
+
+            return $this->redirectToRoute('app_admin_production_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_production/new.html.twig', [
+            'production' => $production,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_production_show', methods: ['GET'])]
+    public function show(Production $production): Response
+    {
+        return $this->render('admin_production/show.html.twig', [
+            'production' => $production,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_production_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Production $production, ProductionRepository $productionRepository): Response
+    {
+        $form = $this->createForm(Production1Type::class, $production);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productionRepository->add($production, true);
+
+            return $this->redirectToRoute('app_admin_production_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_production/edit.html.twig', [
+            'production' => $production,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_production_delete', methods: ['POST'])]
+    public function delete(
+        Request $request,
+        Production $production,
+        ProductionRepository $productionRepository
+    ): Response {
+        if ($this->isCsrfTokenValid('delete' . $production->getId(), $request->request->get('_token'))) {
+            $productionRepository->remove($production, true);
+        }
+
+        return $this->redirectToRoute('app_admin_production_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/Production1Type.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Production;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Production1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Production::class,
+        ]);
+    }
+}
