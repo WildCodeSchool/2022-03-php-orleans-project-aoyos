80b8b93142165c3ea6fdda9d31f272f021f8161a
Ysedda <arthur.baudoin@icloud.com>
2022-06-09 16:54:02 +0200

    form verification and view

--- src/Entity/Partner.php
+++ src/Entity/Partner.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\PartnerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PartnerRepository::class)]
 class Partner
@@ -14,9 +15,18 @@ class Partner
     private int $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        max: 255
+    )]
     private string $name;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Url]
+    #[Assert\Length(
+        max: 255
+    )]
     private string $image;
 
     public function getId(): ?int
