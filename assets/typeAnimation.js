8cfc3b48983cd33ac8e060dae89c7804371b4186
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-09 16:58:01 +0200

    effet type and delete sur image de  bannière ok

---
+++ assets/typeAnimation.js
@@ -0,0 +1,39 @@
+const words = ["Afterwork.", "Séminaire d'entreprise.", "Soirée corporate.", "Soirée corporate."];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            timer = setTimeout(deletingEffect, 1000);
+            return false;
+        };
+        timer = setTimeout(loopTyping, 90);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 150);
+    };
+    loopDeleting();
+};
+
+typingEffect();
