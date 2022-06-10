0963a64085854e09abd28c63c1a17792c3b65632
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-10 09:41:04 +0200

    typage du formulaire

--- src/Form/TeamMemberType.php
+++ src/Form/TeamMemberType.php
@@ -6,16 +6,26 @@ use App\Entity\TeamMember;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TeamMemberType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('position')
-            ->add('description')
-            ->add('picture')
+            ->add('name', TextType::class, [
+                    'label' => 'Nom'
+                ])
+            ->add('position', TextType::class, [
+                'label' => 'Poste occupé'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('picture', TextType::class, [
+                'label' => 'Photo'
+            ])
         ;
     }
 
