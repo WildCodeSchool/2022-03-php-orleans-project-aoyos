9253fc9ac8daf678959a89b5325c8bbfe0bb1538
VanTej <vanessatejedor@gmail.com>
2022-06-11 13:29:38 +0200

    modify type of createdAt

---
+++ migrations/Version20220611111633.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220611111633 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article CHANGE created_at created_at VARCHAR(10) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+}
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -23,12 +23,11 @@ class Article
     )]
     private string $title;
 
-    #[ORM\Column(type: 'datetime')]
-    /**
-     * @var string A "Y-m-d H:i:s" formatted value
-     */
-    #[Assert\DateTime]
-    private DateTime $createdAt;
+    #[ORM\Column(type: 'string', length: 10)]
+    #[Assert\Length(
+        max: 10
+    )]
+    private string $createdAt;
 
     #[ORM\Column(type: 'text')]
     #[Assert\NotBlank]
@@ -66,12 +65,12 @@ class Article
         return $this;
     }
 
-    public function getCreatedAt(): ?DateTime
+    public function getCreatedAt(): ?string
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(DateTime $createdAt): self
+    public function setCreatedAt(string $createdAt): self
     {
         $this->createdAt = $createdAt;
 
