6ec4ab5ef599f232dcf16dc2a24f056d7421fb94
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-11 10:12:20 +0200

    adding and removing class with js

--- assets/adminSidebar.js
+++ assets/adminSidebar.js
@@ -1,4 +1,23 @@
-const sidebar = document.getElementById('offcanvasScrolling');
+const sideBar = document.getElementById('offcanvasScrolling');
+const breakPointWidth = 765;
+
+if (window.innerWidth >= breakPointWidth) {
+    sideBar.classList.add('position-sticky');
+    sideBar.classList.remove('offcanvas');
+    sideBar.classList.remove('offcanvas-start');
+}
 
 window.addEventListener('resize', () => {
+    if (window.innerWidth <= breakPointWidth) {
+        sideBar.classList.add('offcanvas');
+        sideBar.classList.add('offcanvas-start');
+        sideBar.classList.add('position-fixed');
+        sideBar.classList.remove('position-sticky');
+    } else {
+        sideBar.classList.remove('offcanvas');
+        sideBar.classList.remove('offcanvas-start');
+        sideBar.classList.remove('position-fixed');
+        sideBar.classList.add('position-sticky');
+        sideBar.style.visibility = 'visible';
+    }
 });
