6efe4f54ac77a4cb892d4da6c3ea6a1cf18ef32d
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-11 17:33:58 +0200

    controller and indentation in index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductionRepository $productionRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $productions = $productionRepository->findBy([], ['id' => 'DESC']);
+        return $this->render('home/index.html.twig', [
+            'productions' => $productions,
+        ]);
     }
 }
