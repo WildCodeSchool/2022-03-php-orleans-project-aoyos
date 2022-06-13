66efe258e6953f7bf2392ecd5bd51e18c281ef6c
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-13 14:05:02 +0200

    fixtures avec vrais images de logo

--- src/DataFixtures/PartnerFixtures.php
+++ src/DataFixtures/PartnerFixtures.php
@@ -9,13 +9,20 @@ use Faker\Factory;
 
 class PartnerFixtures extends Fixture
 {
+    public const PARTNERSIMG = [
+        'https://aoyos.fr/wp-content/uploads/2022/01/Sans-titre-1-1-300x300.png',
+        'https://aoyos.fr/wp-content/uploads/2022/01/Sans-titre-1-2-300x300.png',
+        'https://aoyos.fr/wp-content/uploads/2022/01/Sans-titre-1-3-300x300.png',
+        'https://aoyos.fr/wp-content/uploads/2022/01/Sans-titre-1-300x300.png',
+        'https://aoyos.fr/wp-content/uploads/2022/01/Sans-titre-1-5-300x300.png'];
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i <= 4; $i++) {
             $partner = new Partner();
             $partner->setName($faker->name());
-            $partner->setImage('https://picsum.photos/100');
+            $partner->setImage(self::PARTNERSIMG[$i]);
             $manager->persist($partner);
         }
         $manager->flush();
