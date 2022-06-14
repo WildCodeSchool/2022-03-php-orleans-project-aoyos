78ac6a6c4164343997793d7a39b06320e76341b1
Ysedda <arthur.baudoin@icloud.com>
2022-06-14 17:06:01 +0200

    fixed migration

--- migrations/Version20220614150524.php
+++ migrations/Version20220614150524.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220614145421 extends AbstractMigration
+final class Version20220614150524 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,15 +21,11 @@ final class Version20220614145421 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE artist (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, artistname VARCHAR(255) NOT NULL, birthdate DATE NOT NULL, idcard VARCHAR(255) NOT NULL, idphoto VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, mail VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, siret VARCHAR(255) NOT NULL, kbis VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME NOT NULL');
-        $this->addSql('ALTER TABLE production CHANGE image image VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE artist');
-        $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE production CHANGE image image VARCHAR(255) DEFAULT NULL');
     }
 }
