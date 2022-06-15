d5ce5f346ec15aedfdf35403f7f7927ade0c6fb9
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-15 16:16:09 +0200

    fix migration

--- migrations/Version20220615120123.php
+++ migrations/Version20220615120123.php
@@ -21,15 +21,11 @@ final class Version20220615120123 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, lastname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, company VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME NOT NULL');
-        $this->addSql('ALTER TABLE production CHANGE image image VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE reservation');
-        $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE production CHANGE image image VARCHAR(255) DEFAULT NULL');
     }
 }
