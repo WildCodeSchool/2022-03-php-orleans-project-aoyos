23a4e853bba53e75f8eb11bb4a4eb412c38ee4ee
Ysedda <arthur.baudoin@icloud.com>
2022-06-15 16:31:46 +0200

    fixed fixtures

--- src/DataFixtures/MusicalStyleFixtures.php
+++ src/DataFixtures/MusicalStyleFixtures.php
@@ -8,14 +8,15 @@ use Doctrine\Persistence\ObjectManager;
 
 class MusicalStyleFixtures extends Fixture
 {
+    public const MUSICALSTYLES = ['Généraliste', 'Chanteur', 'Soul', 'Musique DJ saxophoniste',
+    'House', 'Deep house', 'Électro', 'Pop/folk', 'Musique swing', 'Rock', 'Rap', 'Hip-Hop',
+    'Groove', 'Musique latino', 'Funk'];
+
     public function load(ObjectManager $manager): void
     {
-        $musicalStyles = ['Généraliste', 'Chanteur', 'Soul', 'Musique DJ saxophoniste',
-        'House', 'Deep house', 'Électro', 'Pop/folk', 'Musique swing', 'Rock', 'Rap', 'Hip-Hop',
-        'Groove', 'Musique latino', 'Funk'];
-        for ($i = 0; $i < count($musicalStyles); $i++) {
+        for ($i = 0; $i < count(self::MUSICALSTYLES); $i++) {
             $musicalStyle = new MusicalStyle();
-            $musicalStyle->setName($musicalStyles[$i]);
+            $musicalStyle->setName(self::MUSICALSTYLES[$i]);
             $manager->persist($musicalStyle);
         }
         $manager->flush();
