4f3f29fbcd4cf911072073c469529c89044600a9
Ysedda <arthur.baudoin@icloud.com>
2022-06-15 15:55:30 +0200

    fixed migration

--- migrations/Version20220615131447.php
+++ migrations/Version20220615131447.php
@@ -21,13 +21,11 @@ final class Version20220615131447 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE musical_style (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE production CHANGE image image VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE musical_style');
-        $this->addSql('ALTER TABLE production CHANGE image image VARCHAR(255) DEFAULT NULL');
     }
 }
