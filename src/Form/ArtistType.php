8f0e589aef3d76452cdb2a541ebef26c0eb324a2
Ysedda <arthur.baudoin@icloud.com>
2022-06-15 14:51:50 +0200

    fixed types in type

--- src/Form/ArtistType.php
+++ src/Form/ArtistType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\Artist;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,30 +19,30 @@ class ArtistType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', null, [
+            ->add('firstname', TextType::class, [
             'label' => 'Prénom',
             'attr' => ['placeholder' => 'Gaëtan'],
             'constraints' => [new Length(['max' => 255]), new NotBlank()],
             ])
-            ->add('lastname', null, ['label' => 'Nom',
+            ->add('lastname', TextType::class, ['label' => 'Nom',
             'attr' => ['placeholder' => 'Laurent'],
             'constraints' => [new Length(['max' => 255]), new NotBlank()],
             ])
-            ->add('birthdate', null, ['label' => 'Date de naissance',
+            ->add('birthdate', BirthdayType::class, ['label' => 'Date de naissance',
             'years' => range(date('Y') - 15, (date('1940'))),
             'constraints' => [new NotBlank()],
             ])
-            ->add('phone', null, [
+            ->add('phone', TextType::class, [
             'label' => 'Téléphone',
             'attr' => ['placeholder' => '01 70 06 05 70'],
             'constraints' => [new Length(['max' => 255]), new NotBlank()],
             ])
-            ->add('email', null, [
+            ->add('email', EmailType::class, [
             'label' => 'Email',
             'attr' => ['placeholder' => 'hello@aoyos.fr'],
             'constraints' => [new Length(['max' => 255]), new NotBlank(), new Email()],
             ])
-            ->add('address', null, [
+            ->add('address', TextType::class, [
                 'label' => 'Adresse',
                 'attr' => ['placeholder' => '15 rue des Halles, 75 001 Paris'],
                 'constraints' => [new Length(['max' => 255]), new NotBlank()],
