46b49efe3b60251c2083fa8e208f62da33af4698
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-16 09:25:41 +0200

    corrections

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,7 +18,6 @@ class LoginController extends AbstractController
          // last username entered by the user
          $lastUsername = $authenticationUtils->getLastUsername();
          return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
             'error'         => $error,
          ]);
