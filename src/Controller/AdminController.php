db7fdead99f38455d6f8ffc749da503ae1175062
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-16 11:43:40 +0200

    affichage des djs ok

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin', name: 'admin')]
 class AdminController extends AbstractController
 {
+    public const MAX_ARTISTS = 4;
+
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ArtistRepository $artistRepository): Response
     {
-        return $this->render('admin/index.html.twig');
+        return $this->render('admin/index.html.twig', [
+            'artists' => $artistRepository->findBy([], ['id' => 'DESC'], self::MAX_ARTISTS),
+        ]);
     }
 }
