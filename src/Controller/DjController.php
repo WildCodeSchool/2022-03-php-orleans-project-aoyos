177e7a7636aeba61a5a3b7515feca13c60520149
Ysedda <arthur.baudoin@icloud.com>
2022-06-16 10:15:31 +0200

    fixed gitignore, merged controllers and fixed link

--- src/Controller/DjController.php
+++ src/Controller/DjController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use App\Form\ArtistType;
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,5 +17,26 @@ class DjController extends AbstractController
     public function index(): Response
     {
         return $this->render('dj/index.html.twig');
+    }
+
+    #[Route('/inscription', name: 'registration')]
+    public function register(HttpFoundationRequest $request, ArtistRepository $artistRepository): Response
+    {
+        $artist = new Artist();
+        $form = $this->createForm(ArtistType::class, $artist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $artistRepository->add($artist, true);
+
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm(
+            'dj/registration/index.html.twig',
+            [ 'form' => $form,
+             'artist' => $artist
+            ]
+        );
     }
 }
