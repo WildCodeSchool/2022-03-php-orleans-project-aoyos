3c9b5f60447679d46e6cf10463b94e630c84b68b
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-16 10:40:26 +0200

    Form

---
+++ src/Form/ReservationEventInfosType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reservation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationEventInfosType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('formula', ChoiceType::class, [
+                'label' => 'Formule',
+                'choices'  => [
+                    'Solo' => 'Solo',
+                    'Standard' => 'Standard',
+                    'Sur mesure' => 'Sur mesure'
+                ]
+            ])
+            ->add('eventType', TextType::class, [
+                'label' => 'Type d\'évènement'
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Lieu'
+            ])
+            ->add('dateStart', DateTimeType::class, [
+                'label' => 'Début'
+            ])
+            ->add('dateEnd', DateTimeType::class, [
+                'label' => 'Fin'
+            ])
+            ->add('attendees', TextType::class, [
+                'label' => 'Participants'
+            ])
+            ->add('comment', TextType::class, [
+                'label' => 'Commentaire (optionnel)'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+            'validation_groups' => ['eventInfos'],
+        ]);
+    }
+}
