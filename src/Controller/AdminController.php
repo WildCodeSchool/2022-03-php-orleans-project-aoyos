cfdb98586c876b09409eab3a1f7c742c0f6590a4
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-16 15:08:20 +0200

    style page accueil admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Artist;
 use App\Repository\ArtistRepository;
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin', name: 'admin')]
 class AdminController extends AbstractController
 {
-    public const MAX_ARTISTS = 4;
+    public const MAX_ELEMENTS = 4;
 
     #[Route('/', name: 'index')]
-    public function index(ArtistRepository $artistRepository): Response
+    public function index(ArtistRepository $artistRepository, ReservationRepository $reservationRepo): Response
     {
         return $this->render('admin/index.html.twig', [
-            'artists' => $artistRepository->findBy([], ['id' => 'DESC'], self::MAX_ARTISTS),
+            'artists' => $artistRepository->findBy([], ['id' => 'DESC'], self::MAX_ELEMENTS),
+            'reservations' => $reservationRepo->findBy([], ['id' => 'DESC'], self::MAX_ELEMENTS),
         ]);
     }
 }
