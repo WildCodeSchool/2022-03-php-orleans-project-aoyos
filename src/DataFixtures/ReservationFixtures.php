a72b57e3576c92da3b183ccc2c02521cea90eff8
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-16 20:43:31 +0200

    updated fixtures for reservation

--- src/DataFixtures/ReservationFixtures.php
+++ src/DataFixtures/ReservationFixtures.php
@@ -9,11 +9,18 @@ use Faker\Factory;
 
 class ReservationFixtures extends Fixture
 {
+    public const FORMULAS = [
+        'Solo',
+        'Standard',
+        'Sur mesure',
+    ];
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
         $numberReservations = 10;
+        $totalFormulas = count(self::FORMULAS);
 
         for ($i = 0; $i < $numberReservations; $i++) {
             $reservation = new Reservation();
@@ -22,6 +29,13 @@ class ReservationFixtures extends Fixture
             $reservation->setCompany($faker->words(2, true));
             $reservation->setEmail($faker->email());
             $reservation->setPhone($faker->phoneNumber());
+            $reservation->setFormula(self::FORMULAS[$faker->numberBetween(0, $totalFormulas - 1)]);
+            $reservation->setEventType($faker->sentence(4));
+            $reservation->setAddress($faker->address());
+            $reservation->setDateStart($faker->dateTimeInInterval('+1 week', '+1 days'));
+            $reservation->setDateEnd($faker->dateTimeInInterval('+1 week', '+2 days'));
+            $reservation->setAttendees($faker->randomNumber(3, true));
+            $reservation->setComment($faker->paragraph());
             $manager->persist($reservation);
         }
 
