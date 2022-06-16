47cdbefcdc9a3c1fab61671ce24969dcdf5b4e09
VanTej <vanessatejedor@gmail.com>
2022-06-16 16:36:09 +0200

    validation contact form and sending mail contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,18 +3,38 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+
+            $email = (new Email())
+                ->from($contactFormData['email'])
+                ->to($this->getParameter('mailer_from'))
+                ->subject('Un message depuis le site aoyos')
+                ->html($this->renderView('contact/new_email.html.twig', [
+                    'contactFormData' => $contactFormData,
+                ]));
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+
+            return $this->redirectToRoute('app_contact', [], Response::HTTP_SEE_OTHER);
+        }
 
         return $this->renderForm('contact/index.html.twig', [
             'form' => $form,
