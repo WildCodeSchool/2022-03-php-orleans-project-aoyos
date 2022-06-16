78385d443271cb9515e309ec50de97d3b7786891
VanTej <vanessatejedor@gmail.com>
2022-06-16 14:44:22 +0200

    create and style contactPage

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        return $this->renderForm('contact/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
