0d28971cb28ab79eae6f8f3052bcfa1034e701a0
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-16 11:13:47 +0200

    controller with session managing

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Reservation;
 use App\Form\ReservationClientInfosType;
+use App\Form\ReservationEventInfosType;
 use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,20 +16,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class ClientController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(Request $request, ReservationRepository $reservationRepo): Response
-    {
+    public function index(
+        Request $request,
+        RequestStack $requestStack,
+        ReservationRepository $reservationRepo
+    ): Response {
         $reservation = new Reservation();
-        $form = $this->createForm(ReservationClientInfosType::class, $reservation);
 
+        $step = 0;
+        $session = $requestStack->getSession();
+        if ($session->has('step') && $session->get('step') === 1) {
+            $step = 1;
+            $form = $this->createForm(ReservationEventInfosType::class, $reservation);
+        } else {
+            $form = $this->createForm(ReservationClientInfosType::class, $reservation);
+        }
+        
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $reservationRepo->add($reservation, true);
-            return $this->redirectToRoute('client_index', [], Response::HTTP_SEE_OTHER);
+            if ($step === 1) {
+                $reservation->setLastname($session->get('lastname'));
+                $reservation->setFirstname($session->get('firstname'));
+                $reservation->setCompany($session->get('company'));
+                $reservation->setEmail($session->get('email'));
+                $reservation->setPhone($session->get('phone'));
+                $reservationRepo->add($reservation, true);
+            } else {
+                $reservationSession = $form->getData();
+                $reservationSession['step'] = 1;
+                $session->set('reservationForm', $reservationSession);
+                return $this->redirectToRoute('client_index', [], Response::HTTP_SEE_OTHER);
+            }
         }
 
         return $this->renderForm('client/index.html.twig', [
-            'form' => $form
+            'form' => $form,
+            'step' => $step
         ]);
     }
 }
