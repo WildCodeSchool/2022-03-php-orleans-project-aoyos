e82a4b2015846e272a35e3ce41a0ad77e70eb128
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-20 18:52:41 +0200

    redirect route controller

---
+++ src/Controller/RedirectionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RedirectionController extends AbstractController
+{
+    #[Route('/redirection', name: 'redirect')]
+    public function index(): response
+    {
+        if (in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+            return $this->redirectToRoute('adminindex', [], Response::HTTP_SEE_OTHER);
+        } elseif (in_array('ROLE_USER', $this->getUser()->getRoles(), true)) {
+            return $this->redirectToRoute('app_dj', [], Response::HTTP_SEE_OTHER);
+        } else {
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        }
+    }
+}
