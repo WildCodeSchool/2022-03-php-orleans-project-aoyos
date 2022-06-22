522b8f15490e20129e8ff5ea0d0e391b234c5290
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-22 17:13:18 +0200

    fix d'erreur de commit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin', name: 'admin_')]
+#[Route('/admin', name: 'admins_')]
 class AdminController extends AbstractController
 {
     public const MAX_ELEMENTS = 3;
