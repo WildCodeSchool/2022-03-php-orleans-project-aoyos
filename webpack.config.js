5e1a75fb25edf5fb31283b15af61e52b18613c7d
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-22 15:21:50 +0200

    topbar et sidebar style et structure ok

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ Encore
     .addEntry('dj', './assets/styles/dj.scss')
     .addEntry('login', './assets/styles/login.scss')
     .addEntry('contact', './assets/styles/contact.scss')
+    .addEntry('djDashboard', './assets/styles/djDashboard.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
