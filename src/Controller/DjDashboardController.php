e8ca322af7c1d92f8b8840167b5e0403faf6b8ec
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-23 16:28:07 +0200

    réservations envoyées à la vue et style

--- src/Controller/DjDashboardController.php
+++ src/Controller/DjDashboardController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Repository\ArtistRepository;
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route('/espace-dj', name: 'dashboard_dj_')]
 class DjDashboardController extends AbstractController
 {
+    public const MAX_ELEMENTS = 3;
+
     #[Route('/', name: 'index')]
-    public function index(): Response
-    {
-        return $this->render('dj_dashboard/index.html.twig');
+    public function index(
+        ReservationRepository $reservationRepo,
+        AuthenticationUtils $authenticationUtils,
+        ArtistRepository $artistRepository
+    ): Response {
+        $emailArtist = $authenticationUtils->getLastUsername();
+        $artist = $artistRepository->findOneBy(['email' => $emailArtist]);
+
+        return $this->render('dj_dashboard/index.html.twig', [
+            'reservations' => $reservationRepo->findBy(
+                ['artist' => $artist->getId()],
+                ['id' => 'desc'],
+                self::MAX_ELEMENTS
+            ),
+            'newReservations' => $reservationRepo->findBy([], ['id' => 'desc'], self::MAX_ELEMENTS)
+        ]);
     }
 }
