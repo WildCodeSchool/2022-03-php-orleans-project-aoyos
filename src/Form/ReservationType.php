1ec39ebfe73daebd9c6971ae2bf61e6487953f9d
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-23 10:18:56 +0200

    form

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form;
 
+use App\Config\ReservationStatus;
 use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +18,45 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('company')
-            ->add('email')
-            ->add('phone')
-            ->add('formula')
-            ->add('eventType')
-            ->add('address')
-            ->add('dateStart')
-            ->add('dateEnd')
-            ->add('attendees')
-            ->add('comment')
-            ->add('status')
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Société'
+            ])
+            ->add('email', EmailType::class)
+            ->add('phone', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('formula', TextType::class, [
+                'label' => 'Formule'
+            ])
+            ->add('eventType', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('dateStart', DateTimeType::class, [
+                'label' => 'Début',
+                'widget' => 'single_text',
+            ])
+            ->add('dateEnd', DateTimeType::class, [
+                'label' => 'Fin',
+                'widget' => 'single_text',
+            ])
+            ->add('attendees', NumberType::class, [
+                'label' => 'Participants'
+            ])
+            ->add('comment', TextType::class, [
+                'label' => 'Commentaire'
+            ])
+            ->add('status', EnumType::class, [
+                'class' => ReservationStatus::class
+            ])
         ;
     }
 
