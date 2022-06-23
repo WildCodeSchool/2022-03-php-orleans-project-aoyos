3d0d61ea61c7ad1fc0f9f2e7f4cc39f03f74c563
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-06-23 17:14:42 +0200

    clean up 'use' in form

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,19 +4,14 @@ namespace App\Form;
 
 use App\Config\ReservationStatus;
 use App\Entity\Reservation;
-use BackedEnum;
-use Closure;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use UnitEnum;
 
 class ReservationType extends AbstractType
 {
