8bbbfe31c4e157254b216edf0db7598ce4c47ade
Ysedda <arthur.baudoin@icloud.com>
2022-06-27 12:39:21 +0200

    fixed image size

--- src/Form/DocumentType.php
+++ src/Form/DocumentType.php
@@ -7,22 +7,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class DocumentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('identityCardFile', VichFileType::class, [
+            ->add('identityCardFile', VichImageType::class, [
                 'label' => 'Carte d\'identité',
+                'download_uri' => false,
+                'delete_label' => 'Supprimer ?',
             ])
-            ->add('identityPhotoFile', VichFileType::class, [
+            ->add('identityPhotoFile', VichImageType::class, [
                 'label' => 'Photo d\'identité',
+                'download_uri' => false,
+                'delete_label' => 'Supprimer ?',
             ])
-            ->add('kbisFile', VichFileType::class, [
+            ->add('kbisFile', VichImageType::class, [
                 'label' => 'KBIS',
-                'required' => false
+                'required' => false,
+                'download_uri' => false,
+                'delete_label' => 'Supprimer ?',
             ])
             ->add('siretNumber', TypeTextType::class, [
                 'label' => 'SIRET',
