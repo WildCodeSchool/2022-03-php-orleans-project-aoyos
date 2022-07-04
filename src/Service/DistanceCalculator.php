e0bf083fee2b5916ee16e300a98c87426bab8148
Ysedda <arthur.baudoin@icloud.com>
2022-07-04 16:01:22 +0200

    interface and calculation

--- src/Service/DistanceCalculator.php
+++ src/Service/DistanceCalculator.php
@@ -9,6 +9,7 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class DistanceCalculator
 {
     public const URL = 'https://api-adresse.data.gouv.fr/search/';
+    public const EARTH_RADIUS_KM = 6371.07103;
 
     public function __construct(
         private HttpClientInterface $client,
@@ -39,5 +40,17 @@ class DistanceCalculator
         } else {
             throw new Exception('Une erreur est survenue lors de la récupération de l\'adresse');
         }
+    }
+
+    public function getDistance(Localizable $start, Localizable $end): float
+    {
+        $radiusLatitudeStart = $start->getLatitude() * (pi() / 180);
+        $radiusLatitudeEnd = $end->getLatitude() * (pi() / 180);
+        $latitudeDifference = $radiusLatitudeEnd - $radiusLatitudeStart;
+        $longitudeDifference = ($end->getLongitude() - $start->getLongitude()) * (pi() / 180);
+        $distance = 2 * self::EARTH_RADIUS_KM * sin(sqrt(sin($latitudeDifference / 2) * sin($latitudeDifference / 2)
+        + cos($radiusLatitudeStart) * cos($radiusLatitudeEnd)
+        * sin($longitudeDifference / 2) * sin($longitudeDifference / 2)));
+        return $distance;
     }
 }
