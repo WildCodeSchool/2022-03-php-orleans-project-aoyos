9035cb740f04ab8a99b528e8bed0c6634c419f8d
VanTej <vanessatejedor@gmail.com>
2022-07-06 12:02:57 +0200

    modify controller and add findbyrole

--- src/Controller/TeamMemberController.php
+++ src/Controller/TeamMemberController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\TeamMemberRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeamMemberController extends AbstractController
 {
     #[Route('/equipe', name: 'app_team')]
-    public function index(TeamMemberRepository $teamMemberRepository): Response
+    public function index(TeamMemberRepository $teamMemberRepository, UserRepository $userRepository): Response
     {
+        $artists = $userRepository->findByRole('ROLE_DJ');
+
         return $this->render('team/index.html.twig', [
             'team_members' => $teamMemberRepository->findBy([], ['name' => 'ASC']),
+            'artists' => $artists,
         ]);
     }
 }
